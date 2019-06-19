062c59403b22c8b8af1a72cd31faa0fcb5d065c4
Jose Bujalance <joseanbu23@gmail.com>
2019-06-19 19:11:26 +0200

    Remove useless resources

--- app/src/main/java/io/github/muzplash/ui/settings/SettingsActivity.kt
+++ app/src/main/java/io/github/muzplash/ui/settings/SettingsActivity.kt
@@ -1,8 +1,9 @@
-package io.github.muzplash
+package io.github.muzplash.ui.settings
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import io.github.muzplash.R
 
 class SettingsActivity : AppCompatActivity() {
 
